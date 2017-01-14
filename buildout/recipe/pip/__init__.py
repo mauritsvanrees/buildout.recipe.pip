32740173666dd2dbf9c78db5d079abcf821848c3
Maurits van Rees <maurits@vanrees.org>
2017-01-14 19:46:48 +0100

    First simplistic virtualenv and pip call.

--- buildout/recipe/pip/__init__.py
+++ buildout/recipe/pip/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 """Recipe pip"""
+from subprocess import call
+
+import os
+import virtualenv
+
 
 class Recipe(object):
     """zc.buildout recipe"""
@@ -9,7 +14,16 @@ class Recipe(object):
 
     def install(self):
         """Installer"""
-        # XXX Implement recipe functionality here
+        env_dir = os.path.join(self.buildout['buildout']['parts-directory'], self.name)
+        if not os.path.exists(env_dir):
+            virtualenv.create_environment(env_dir)
+        pip_script = os.path.join(env_dir, 'bin', 'pip')
+        packages = self.options.get('packages', self.options.get('eggs'), '').split()
+        if not packages:
+            return tuple()
+        pip_command = [pip_script, 'install']
+        pip_command.extend(packages)
+        call(pip_command)
 
         # Return files that were created by the recipe. The buildout
         # will remove all returned files upon reinstall.
